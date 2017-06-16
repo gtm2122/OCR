f092d0056bddb745222b80a1288eff560a047067
gtm2122 <gtm2122@github.com>
2017-06-16 12:37:54 -0400

    added an empy augmentation function with comments

---
+++ augment.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import skimage
+import scipy
+import numpy
+
+##### This function randomly augments data
+##### The directory containing images could be passed as input
+##### In the end this function should also ensure that the number of images
+##### after augmenting should be the same for all classes
+def aug(folder):
+    
+    
+    
+    
+    ## Scaling
+    
+    ## Salt and pepper
+    
+    ## Slash inpainting ?
+    
+    ## Gaussian noise ?
+    
+    ## save images.
+    
+    raise NotImplementedError
+    
